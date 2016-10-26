092701ad43c4b836f2a124a0683b1999c95e704e
Robert Arin <robbie.arin@gmail.com>
2016-10-26 13:50:52 -0700

    Nav bar bugfix

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,6 @@
 exports.view = function(req, res){
   res.render('Science', {
   	'pageName': "Science",
-  	'links': [
-		{	'name':	'Science',	
-			'image':	'science.jpg',	
-			'id':	'science'
-		},	
-		{	'name': 'Tasks',
-	  		'image': 'tasks.jpg',
-	  		'id': 'task'
-	  	},
-		{	'name':	'Alarms',	
-			'image':	'alarms.jpg',	
-			'id':	'alarms'	
-		},
   	]
   	
   });
--- routes/page.js
+++ routes/page.js
@@ -2,19 +2,5 @@ exports.viewPage = function(req, res) {
 	var name = req.params.name;
 	res.render(name, {
 		'pageName': name,
-		'links': [
-		{	'name':	'Science',	
-			'image':	'science.jpg',	
-			'id':	'science'
-		},	
-		{	'name': 'Tasks',
-	  		'image': 'tasks.jpg',
-	  		'id': 'task'
-	  	},
-		{	'name':	'Alarms',	
-			'image':	'alarms.jpg',	
-			'id':	'alarms'	
-		},
-  	]
 	});
 };
