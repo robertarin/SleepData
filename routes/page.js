ef5d0331bfc3cb2b1d0636aa835af2006a7f2f62
Robert Arin <robbie.arin@gmail.com>
2016-10-27 00:02:20 -0700

    Added some new css and login page

--- routes/page.js
+++ routes/page.js
@@ -1,6 +1,20 @@
 exports.viewPage = function(req, res) {
 	var name = req.params.name;
 	res.render(name, {
-		'pageName': name
+		'pageName': name,
+		'alarms': [
+	  	{	'name': 'Work',
+	  		'time': '7am',
+	  		'id': 'workAlarm'
+	  	},
+		{	'name':	'Class',	
+			'time': '11am',	
+			'id':	'classAlarm'
+		},	
+		{	'name':	'Dinner',	
+			'time':	'5pm',	
+			'id':	'dinnerAlarm'	
+		},
+  	]
 	});
 };
