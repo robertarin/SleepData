4a290df0d5618974cd616ad97a278fd1c82f24a9
Robert Arin <robbie.arin@gmail.com>
2016-10-27 13:47:16 -0700

    New alarm functionality

--- public/js/page.js
+++ public/js/page.js
@@ -9,4 +9,22 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$("#subBtn").click(subBtnClick);
+}
+
+function subBtnClick(e)
+{
+	e.preventDefault();
+
+	var nameField = $("#nameField");
+	var name = nameField.val();
+	nameField.val("");
+
+	var timeField = $("#timeField");
+	var time = timeField.val();
+	timeField.val("");
+
+	var alarms = $("#alarms");
+	alarms.append("<li><p>"+name+": "+time+"</p><label class='switch'><input type='checkbox'><div class='slider'></div></label></li>");
+
 }
