8b58ca1542519f7535678266f5592b0fa1419372
Robert Arin <robbie.arin@gmail.com>
2016-11-17 00:17:16 -0800

    Case studies own page

--- public/js/case.js
+++ public/js/case.js
@@ -8,15 +8,20 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#submitGlobal").click(submitGlobal);
+	$("#submitCase").click(submitCase);
 }
 
-function submitGlobal() {
-	$("#answerGlobal").val("");
-	$("#commentsGlobal").val("");
-	$("#alertGlobal").show();
-	//$("#submitGlobal").hide();
+function submitCase() {
+	var caseRadio = $('#caseRadio input:radio:checked').val();
+
+	$("input:radio").prop('checked', false);
+    $("input:radio").closest("label").removeClass("active");
+
+	$("#commentsCase").val("");
+	$("#alertCase").show();
+	//$("#alertCase").hide();
 }
+
 
 $(function(){
     $("[data-hide]").on("click", function(){
